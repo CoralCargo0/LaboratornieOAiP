7618fc218a8d62e37169ba79e5737574c3d24ab6
Trokay Oleg <59147112+CoralCargo0@users.noreply.github.com>
2020-10-14 22:44:19 +0300

    Add files via upload

---
+++ Laboratornaya5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include<locale.h>
+#define n 5
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");//Русификация вывода
+	int i, d, k, supermas[n][n], ms[2*n],error;
+	printf_s("Введите числa \n");
+	for (i = 0; i < n; i++) // Ввод массива
+	{
+		do{
+		error = scanf_s("%d", &ms[i]);  //Проверка ввода 
+		if (error == 0)
+		{
+			printf("Ошибка, пожалуйста введите число\n");
+			rewind(stdin);
+		}
+		  } while (error == 0);
+	}for (d = 0; d < n; d++)      
+	{
+		ms[d + n] = ms[d];
+	}
+	printf("Матрица заданного вида:\n");
+	for (k= 0; k < n; k++) //Процесс создания искомой матрицы
+	{
+		for (i = 0; i < n; i++)
+		{
+			supermas[k][i] = ms[i + k];
+			printf("%d ", supermas[k][i]);
+		}
+		printf("\n");
+	}
+}
