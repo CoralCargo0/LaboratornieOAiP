69e552e8ddd2703e4adc272fa4d80a090f4755f4
Trokay Oleg <59147112+CoralCargo0@users.noreply.github.com>
2020-11-09 21:55:05 +0300

    Add files via upload

---
+++ Laboratornaya7.c
@@ -0,0 +1,58 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <iostream>
+int main()
+{
+	system("chcp 1251");
+	system("cls");
+	char* string;
+	int strsize, i = 1 , error ;
+	do                                               //Выделение памяти + ввод размера строки + проверка на правильность ввода и достаточность памяти
+	{
+		do
+		{
+		  rewind(stdin);
+		  printf(" Задайте размер строки: ");
+		  error = scanf_s("%d", &strsize);
+		  strsize+=2;
+	    } while (strsize < 1 || error == 0);
+		string = (char*)calloc(strsize, sizeof(char));
+		if (string==NULL)
+		{
+			printf(" Памяти недостаточно\n");
+		}
+	} while (string == NULL);
+	printf(" Задайте строку: ");
+	error = getchar();
+	if (error == NULL)return 0;
+	fgets(string, strsize, stdin);
+	while (string[i]!='\n')
+	{
+		if (string[i] == '.' || string[i]== '?' || string[i]== '!' )
+		{
+			i++;
+			if (string[i] == '\n')break ;
+			if (string[i] == ' ')i += 2;
+			else i++;
+			continue;
+			
+		}
+		if ( string[i]>=32 && string[i]<= 64 || string[i] == '»' || string[i] == '«')
+		{ 
+			i++;
+			continue;
+		}
+		if (string[i] == 'Ё')
+		{
+			*(string+i) = -72;
+			i++;
+			continue;
+		}
+		string[i] += 32;
+		i++;
+	}
+	printf(" Новая строка: %s\n", string);
+	free(string);
+	return 0;
+}
